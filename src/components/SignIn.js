3c2fe52b89ed26af72ff298b84348c1119d119d1
wingleeio <contact@winglee.io>
2019-06-25 22:32:20 -0700

    created login screen

---
+++ src/components/SignIn.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function SignIn() {
+	return (
+		<div className='panel'>
+			<h1>Login</h1>
+			<p>Enter your username and password to login.</p>
+			<form>
+				<div>
+					<input type='text' placeholder='Enter your username' name='username' />
+				</div>
+				<div>
+					<input type='text' placeholder='Enter your username' name='username' />
+				</div>
+				<button type='submit'>Login</button>
+			</form>
+		</div>
+	);
+}
---
+++ src/components/SignUp.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function SignUp() {
+	return (
+		<div className='panel'>
+			<h1>Register</h1>
+			<p>Enter the following information to create your account.</p>
+			<form>
+				<div>
+					<input type='text' placeholder='Enter your username' name='username' />
+				</div>
+				<div>
+					<input type='text' placeholder='Enter your username' name='username' />
+				</div>
+				<button type='submit'>Register</button>
+			</form>
+		</div>
+	);
+}
